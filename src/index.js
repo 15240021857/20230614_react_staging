4df095f9f9636056aba7378d21e1d27906172900
wright.wu@quectel.com <wright.wu@quectel.com>
2023-06-14 21:27:23 +0800

    熟悉src和public文件夹下文件

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+// 引入React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// 引入根组件
 import App from './App';
+// 浏览器性能分析 --不需要
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // StrictMode严格使用最新版react语言，旧语法会报错不让写
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
